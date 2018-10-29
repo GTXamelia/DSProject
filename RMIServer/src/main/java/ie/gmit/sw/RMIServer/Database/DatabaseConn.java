b6648196d0951bf959b4b307dbf38434bb608f1d
Cian Gannon <cianmagfhionnain@outlook.com>
2018-10-29 14:14:33 +0000

    Fixed errors involving interface and ID

--- RMIServer/src/main/java/ie/gmit/sw/RMIServer/Database/DatabaseConn.java
+++ RMIServer/src/main/java/ie/gmit/sw/RMIServer/Database/DatabaseConn.java
@@ -1,16 +1,24 @@
 package ie.gmit.sw.RMIServer.Database;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class DatabaseConn implements DatabaseConnInterface{
+public class DatabaseConn extends UnicastRemoteObject implements DatabaseConnInterface{
+	
+	public DatabaseConn() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final long serialVersionUID = 1L;
 	
 	private static final String JDBC_DRIVER = "org.h2.Driver";   
    	private static final String DB_URL = "jdbc:h2:~/DSProject";
 
    	private Connection conn;
-
-	@Override
+   	
 	public void Connect() {
 		System.out.println("test");
 	}
--- RMIServer/src/main/java/ie/gmit/sw/RMIServer/Database/DatabaseConnInterface.java
+++ RMIServer/src/main/java/ie/gmit/sw/RMIServer/Database/DatabaseConnInterface.java
@@ -1,9 +1,10 @@
 package ie.gmit.sw.RMIServer.Database;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface DatabaseConnInterface extends Remote{
 	
-	public void Connect();
+	public void Connect() throws RemoteException;
 
 }
