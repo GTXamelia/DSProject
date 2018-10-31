958d4d72002e124f3501acad44dc56f03dd857cd
Cian Gannon <cianmagfhionnain@outlook.com>
2018-10-31 23:29:43 +0000

    Added basic client

---
+++ Client/WebContent/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="ISO-8859-1">
+	<title>Booking Service</title>
+</head>
+
+<body>
+	
+	<form action= "Customers" method="get">
+		<input type="submit" value="Get all Customers"/>
+	</form>
+</body>
+</html>
---
+++ Client/src/ie/gmit/sw/CustomerServlet.java
@@ -0,0 +1,48 @@
+package ie.gmit.sw;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+
+@WebServlet("/Customers")
+public class CustomerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public CustomerServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Client client = Client.create();
+		
+		WebResource wr = client.resource("http://localhost:8080/WebService/webapi/customer/get");
+		
+		String r = wr.accept(MediaType.APPLICATION_JSON).get(String.class);
+		
+		Gson gson=new Gson();
+		
+		Type listType = new TypeToken<ArrayList<Customer>>(){}.getType();
+		
+		List<Customer> customers = gson.fromJson(r, listType);
+
+        request.setAttribute("customers", customers);
+        
+        request.getRequestDispatcher("/WEB-INF/Customers.jsp").forward(request, response);
+	}
+	
+}
