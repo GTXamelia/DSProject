ab03f0e52943fc0c5332a02b38ec1b3c5bc18ea3
Cian Gannon <cianmagfhionnain@outlook.com>
2018-11-28 11:56:26 +0000

    Bookings appearing in web application

---
+++ Client/WebContent/WEB-INF/Bookings.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+     <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Customers</title>
+	</head>
+
+	<body>
+	
+	<table>
+	    <c:forEach items="${bookings}" var="booking">
+	        <tr>
+	        	<td>${booking.id}</td>
+	        	<td>${booking.dateStart}</td>
+	        	<td>${booking.dateEnd}</td>
+	        	<td>${booking.carID}</td>
+	        	<td>${booking.customerID}</td>
+	        	<td>
+		        	<a href="BookingsEdit?id=${booking.id}">
+	        		Edit
+	        		</a>
+	        		<a href="BookingsDelete?id=${booking.id}">
+	        		Delete
+	        		</a>
+	        	</td>
+	        </tr>
+	    </c:forEach>
+	</table>
+	
+	</body>
+</html>
---
+++ Client/src/ie/gmit/sw/bookings/BookingsServlet.java
@@ -0,0 +1,49 @@
+package ie.gmit.sw.bookings;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+
+@WebServlet("/Bookings")
+public class BookingsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public BookingsServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Client client = Client.create();
+		
+		WebResource wr = client.resource("http://localhost:8080/Rest-Server/webapi/booking/get");
+		
+		String r = wr.accept(MediaType.APPLICATION_JSON).get(String.class);
+		
+		Gson gson=new Gson();
+		
+		Type listType = new TypeToken<ArrayList<Bookings>>(){}.getType();
+		
+		List<Bookings> bookings = gson.fromJson(r, listType);
+
+        request.setAttribute("bookings", bookings);
+        
+        request.getRequestDispatcher("/WEB-INF/Bookings.jsp").forward(request, response);
+        
+	}
+	
+}
