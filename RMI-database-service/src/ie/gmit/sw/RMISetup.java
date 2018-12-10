5cea3f1af17e3fb34634d97ce8601d4d344e1e2f
Cian Gannon <cianmagfhionnain@outlook.com>
2018-12-10 20:15:17 +0000

    Commented and formatted RMI server

--- RMI-database-service/src/ie/gmit/sw/RMISetup.java
+++ RMI-database-service/src/ie/gmit/sw/RMISetup.java
@@ -7,20 +7,21 @@ public class RMISetup {
 
 	// Adapted from 'ds wk6 RMI FileServer'
 	public static void main(String[] args) throws Exception {
-		
+
+		// Setup interface
 		DatabaseOption db = new DatabaseOptionImpl();
-		
+
+		// Setup RMI with poer 1099
 		LocateRegistry.createRegistry(1099);
-		
+
+		// Add interface to RMI
 		Naming.rebind("databaseOption", db);
-		
+
+		// Output that the RMI service has started
 		System.out.println("RMI server started...");
-		
+
+		// Generate testing table
 		DatabaseOptionImpl.createTables();
-		
-		// Test DB
-		db.Connect();
-		db.Close();
 	}
-	
+
 }
